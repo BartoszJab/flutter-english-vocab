f87ba0f2c8f73bed4a9b93db61baadf82a2208f3
Bartosz <bartekj1221@wp.pl>
2022-04-07 22:32:04 +0200

    home page update; scaffold messages for loading and error states

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_english_app/bloc/word_search_bloc.dart';
 import 'package:flutter_english_app/repositories/word_repository.dart';
 import 'package:flutter_english_app/views/word_page.dart';
-import 'package:flutter_english_app/widgets/common_word_widget.dart';
-import 'package:flutter_english_app/widgets/search_word_widget.dart';
+import 'package:flutter_english_app/widgets/home_page/common_word_widget.dart';
+import 'package:flutter_english_app/widgets/home_page/search_word_widget.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -24,13 +24,14 @@ class _HomePageState extends State<HomePage> {
         body: BlocListener<WordSearchBloc, WordSearchState>(
           listener: (context, state) {
             if (state is WordSearchLoadingState) {
-              print("loading");
+              _showLoadingSnackBar(context);
             } else if (state is WordSearchLoadedState) {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
               Navigator.push(context, MaterialPageRoute(builder: (context) {
                 return WordPage(word: state.word);
               }));
             } else if (state is WordSearchErrorState) {
-              print("error");
+              _showErrorSnackBar(context);
             }
           },
           child: SafeArea(
@@ -38,15 +39,12 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SearchWordWidget(),
-                  const SizedBox(height: 10),
-                  const Text(
+                children: const [
+                  SearchWordWidget(),
+                  SizedBox(height: 20),
+                  Text(
                     'Najczęściej występujące',
                     style: TextStyle(fontSize: 24.0),
-                  ),
-                  const SizedBox(
-                    height: 10,
                   ),
                   CommonWordWidget(),
                 ],
@@ -57,4 +55,14 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+void _showErrorSnackBar(BuildContext context) {
+  const snackBar = SnackBar(content: Text("Wystąpił błąd"));
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+void _showLoadingSnackBar(BuildContext context) {
+  const snackBar = SnackBar(content: Text("Ładowanie..."));
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
 }
